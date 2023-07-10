4f72a3c080027b4377a93c98ea7d6ef2c52ed39f
robertobenfini <roberto.benfini@gmail.com>
2023-07-10 13:36:17 +0200

    first commit

---
+++ Models/Products.php
@@ -0,0 +1,15 @@
+<?php
+
+    class Products {
+        public $name;
+        public $price;
+        public $type;
+
+        function __construct($name, $price, $type){
+            $this -> name = $name;
+            $this -> price = $prie;
+            $this -> type = $type;
+        }
+    }
+
+?>
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+    require_once __DIR__."/Models/Products.php";
+    require_once __DIR__."/Models/Foods.php";
+    require_once __DIR__."/Models/Accessories.php";
+    require_once __DIR__."/Models/Games.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- link bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+        <!-- link fontawesome -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <title>Document</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
